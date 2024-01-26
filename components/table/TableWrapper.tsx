fe478ae975847e3cbf85a670cb712b47695098e9
Arkya Patwa <arkyapatwa333@gmail.com>
2024-01-26 21:02:38 +0530

    skeleton of the data table added

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { skeleton } from "@/typings";
@@ -9,6 +9,7 @@ import { useUser } from "@clerk/nextjs";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const TableWrapper = ({ skeletonFiles }: { skeletonFiles: skeleton[] }) => {
   const { user } = useUser();
@@ -16,11 +17,9 @@ const TableWrapper = ({ skeletonFiles }: { skeletonFiles: skeleton[] }) => {
   const [sort, setSort] = useState<"asc" | "desc">("desc");
 
   const [docs, loading, error] = useCollection(
-    user &&
-      query(
-        collection(db, "users", user.id, "files"),
-        orderBy("timestamp", sort)
-      )
+    user && (
+        query(collection(db, "users", user.id, "files"), orderBy("timestamp", sort))
+    )
   );
 
   useEffect(() => {
@@ -36,18 +35,46 @@ const TableWrapper = ({ skeletonFiles }: { skeletonFiles: skeleton[] }) => {
       size: doc.data().size,
     }));
 
-    setInitialFiles(files)
-
+    setInitialFiles(files);
   }, [docs]);
 
-  
+  if (docs?.docs.length === undefined) {
+    return (
+      <div className="flex flex-col">
+        <Button variant={"outline"} className="w-36 h-10 mb-5 ml-auto">
+          <Skeleton className="h-5 w-full" />
+        </Button>
+
+        <div className="border rounded-lg">
+          <div className="border-b h-12" />
+          {skeletonFiles.map((file) => (
+            <div className="flex items-center w-full p-5 gap-4" key={file.id}>
+              <Skeleton className="w-12 h-12" />
+              <Skeleton className="w-full h-12" />
+            </div>
+          ))}
+
+          {skeletonFiles.length == 0 && (
+            <div className="flex items-center w-full p-5 gap-4" key={file.id}>
+              <Skeleton className="w-12 h-12" />
+              <Skeleton className="w-full h-12" />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <div className="flex flex-row-reverse my-4">
-        <Button
-        onClick={() => sort == "asc" ? setSort("desc") : setSort("asc")}
-        > Sort By: {sort == "asc" ? "Newest" : "Oldest"} </Button>
+      <div className="flex flex-row-reverse my-4 ">
+        <Button variant={"outline"}
+        className="w-36"
+          onClick={() => (sort == "asc" ? setSort("desc") : setSort("asc"))}
+        >
+          {" "}
+          Sort By: {sort == "asc" ? "Newest" : "Oldest"}{" "}
+        </Button>
       </div>
       {/* Data Table */}
       <DataTable columns={columns} data={initialFiles} />
