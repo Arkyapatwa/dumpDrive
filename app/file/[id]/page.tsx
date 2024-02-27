b352af8354aabe0f9fc76b5ddb650509a19e6ecb
Arkya Patwa <arkyapatwa333@gmail.com>
2024-02-27 23:04:00 +0530

    file data fetch done using id

--- app/file/[id]/page.tsx
+++ app/file/[id]/page.tsx
@@ -1,12 +1,47 @@
-
+"use client";
+import { db } from "@/firebase";
 // import { useRouter } from 'next/router'
 
-const FilePage = ({params}: {params: {id:string}}) => {
-    // const router  = useRouter()
-    console.log(params)
-  return (
-    <div> FilePage for {params.id}</div>
-  )
-}
+import { useUser } from "@clerk/nextjs";
+import { collection, doc, getDoc, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import { CollectionHook, useCollection } from "react-firebase-hooks/firestore";
 
-export default FilePage
+const FilePage = ({ params }: { params: { id: string } }) => {
+  // const router  = useRouter()
+  const { user } = useUser();
+  const [file, setFile] = useState<any>(null);
+
+  // console.log(params)
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchFile = async () => {
+      // const fileData = await query(collection(db, "users", user.id, "files", params.id))
+      const fileDataRef = doc(
+        collection(db, `users/${user.id}/files`),
+        params.id
+      );
+      const fileDocSnapshot = await getDoc(fileDataRef);
+      if (fileDocSnapshot) {
+        setFile(fileDocSnapshot._document.data.value.mapValue.fields);
+        console.log(fileDocSnapshot._document.data.value.mapValue.fields);
+      }
+    };
+
+    fetchFile();
+    // console.log(file)
+  }, [user]);
+
+  return (
+    <div>
+      
+      <div>
+        {/* <DocViewer documents={[file.downloadUrl]} pluginRenderers={DocViewerRenderers} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default FilePage;
